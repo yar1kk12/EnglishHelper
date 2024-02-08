f192dd0a5f69b6f2e0162f89974e4335269bf479
Yaroslav <yar1k_k12@icloud.com>
2024-02-08 21:54:43 +0200

    Search Phrasal verbs implemented

--- src/views.py
+++ src/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView
 from src.models import PhrasalVerbs
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,16 +11,34 @@ class PhrasalVerbsView(ListView):
 
     group_by = {start.phrasal_verb.split(' ')[0] for start in PhrasalVerbs.objects.all()}
 
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     search = self.request.GET.get(' search', '')
+    #
+    #     if search:
+    #         queryset = queryset.filter(
+    #             Q(phrasal_verb__icontains=search)
+    #         )
+    #     return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        search = self.request.GET.get('search', '')
         data = {}
 
-        for start in sorted(self.group_by):
-            queryset = PhrasalVerbs.objects.filter(phrasal_verb__istartswith=start)
-            data[start] = queryset
+        if search:
+            queryset = PhrasalVerbs.objects.all()
+            queryset = queryset.filter(
+                Q(phrasal_verb__icontains=search)
+            )
+            context['search'] = queryset
+        else:
+            for start in sorted(self.group_by):
+                queryset = PhrasalVerbs.objects.filter(phrasal_verb__istartswith=start)
+                data[start] = queryset
 
-        context['verbs_dict'] = data
-        context['text'] = 'fsdfs \n fasdf'
+            context['verbs_dict'] = data
 
         return context
 
